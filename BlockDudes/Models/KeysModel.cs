b4e2c4b0d8e20b4bbdc8f9b7ba801e7cb648fb88
dromanchenko <ROMANCHENKO.DMITRIY.91@GMAIL.COM>
2019-04-12 16:21:28 +0300

    adds keys page

---
+++ BlockDudes/Models/KeysModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BlockDudes.Models
+{
+    public class KeysModel
+    {
+        public string PrivateKey { get; set; }
+
+        public string PublicKey { get; set; }
+    }
+}
